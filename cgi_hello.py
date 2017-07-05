837c857c866eab6f64bdb492eeb0e9ed9f181932
Cokrie <595258481@qq.com>
2017-07-05 11:15:50 +0800

    test

---
+++ cgi_hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+print "Content-type:text/html"
+print
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<title>Hello World</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello World!</h2>'
+print '</body>'
+print '</html>'
